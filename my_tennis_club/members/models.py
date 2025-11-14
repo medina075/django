d178036ed8cf78d44bb1093eb9ea041b1a0bb52c
Carlos Eduardo Medina Cepeda <caremedina1@soy.sena.edu.co>
2025-11-14 07:34:04 -0500

    Parte 2 base de datos

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,4 +4,7 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+  city = models.CharField(max_length=200,null=True)
+  genero = models.CharField(max_length=200,null=True)
+  email = models.EmailField(null=True)
 # Create your models here.
